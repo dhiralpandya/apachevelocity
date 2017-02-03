ac219cb30ebe0f434b26bf87b7aa4e58803183f2
dhiralpandya <dhiral_pandya@apple.com>
2017-02-02 21:11:54 -0800

    added

--- OmtVelocity/src/com/omt/velocity/Customer.java
+++ OmtVelocity/src/com/omt/velocity/Customer.java
@@ -6,11 +6,27 @@ public class Customer {
 
 	}
 
-	public String name;
-	public String company;
+	private String name;
+	private String company;
 
 	public String getNameCompany() {
 		return name + "|" + company;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
 	}
 
 }
